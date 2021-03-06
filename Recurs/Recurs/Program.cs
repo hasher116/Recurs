272fba444a5e97420fbc482f3c57210b61721514
hasher116 <andrei13081996@mail.ru>
2021-03-06 06:31:07 +0300

    Initial commit

---
+++ Recurs/Recurs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Recurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(5));
+            Console.WriteLine(fibonachi(12));
+        }
+        static int factorial (int x)
+        {
+            if (x == 0)
+                return 1;
+            else
+                return x * factorial(x - 1);
+        }
+        static int fibonachi(int x)
+        {
+            if (x == 0)
+                return 0;
+            else if (x == 1)
+                return 1;
+            else
+                return fibonachi(x - 1) + fibonachi(x - 2);
+        }
+    }
+}
